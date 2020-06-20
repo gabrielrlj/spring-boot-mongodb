6cad57e676460668065954357a5be9da1fd02a5c
Gabriel Jardim <gabrielrlj99@outlook.com>
2020-06-20 11:55:46 -0300

    projeção dos dados do autor com DTO

--- src/main/java/com/jardim/mongo/config/Instantiation.java
+++ src/main/java/com/jardim/mongo/config/Instantiation.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 
 import com.jardim.mongo.domain.Post;
 import com.jardim.mongo.domain.User;
+import com.jardim.mongo.dto.AuthorDTO;
 import com.jardim.mongo.repositories.PostRepository;
 import com.jardim.mongo.repositories.UserRepository;
 
@@ -35,10 +36,12 @@ public class Instantiation implements CommandLineRunner {
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
 		
-		Post post1 = new Post(null, sdf.parse("21/03/2020"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", maria);
-		Post post2 = new Post(null, sdf.parse("23/03/2020"), "Bom dia", "Acordei putasso", maria);
-		
 		userRepo.saveAll(Arrays.asList(maria, alex, bob));
+		
+		Post post1 = new Post(null, sdf.parse("21/03/2020"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
+		Post post2 = new Post(null, sdf.parse("23/03/2020"), "Bom dia", "Acordei putasso", new AuthorDTO(maria));
+		
+		
 		postRepo.saveAll(Arrays.asList(post1, post2));
 	}
 
