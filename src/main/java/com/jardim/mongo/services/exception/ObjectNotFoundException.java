90ae154ef702fc66e2dcd7e5e3d497a4bd21e21b
Gabriel Jardim <gabrielrlj99@outlook.com>
2020-06-20 10:06:16 -0300

    obtendo um usuário por Id

---
+++ src/main/java/com/jardim/mongo/services/exception/ObjectNotFoundException.java
@@ -0,0 +1,14 @@
+package com.jardim.mongo.services.exception;
+
+public class ObjectNotFoundException extends RuntimeException{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	public ObjectNotFoundException(String msg) {
+		super(msg);
+	}
+
+}
