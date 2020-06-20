2a0e2a7cfd3f76ebb176d299baebec1eb6657f7d
Gabriel Jardim <gabrielrlj99@outlook.com>
2020-06-20 13:12:15 -0300

    endpoint para retornar os posts de um user

--- src/main/java/com/jardim/mongo/resources/UserResource.java
+++ src/main/java/com/jardim/mongo/resources/UserResource.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.jardim.mongo.domain.Post;
 import com.jardim.mongo.domain.User;
 import com.jardim.mongo.dto.UserDTO;
 import com.jardim.mongo.services.UserService;
@@ -61,6 +62,12 @@ public class UserResource {
 			obj.setId(id);
 			obj = service.update(obj);
 			return ResponseEntity.noContent().build();
+		}
+		
+		@GetMapping("/{id}/posts")
+		public ResponseEntity<List<Post>> findPosts(@PathVariable String id){
+			User obj = service.findById(id);
+			return ResponseEntity.ok().body(obj.getPosts());
 		}
 	
 }
