15e5d10df91a81d4647918877aa9587335a09e22
Gabriel Jardim <gabrielrlj99@outlook.com>
2020-06-22 10:50:45 -0300

    consulta com vários critérios

--- src/main/java/com/jardim/mongo/resources/PostResource.java
+++ src/main/java/com/jardim/mongo/resources/PostResource.java
@@ -1,5 +1,6 @@
 package com.jardim.mongo.resources;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -35,5 +36,20 @@ public class PostResource {
 		return ResponseEntity.ok().body(list);
 
 	}
+	
+	@GetMapping("/fullsearch")
+	public ResponseEntity<List<Post>> fullsearch(
+			@RequestParam(value = "text", defaultValue="") String text,
+			@RequestParam(value = "minDate", defaultValue="") String minDate,
+			@RequestParam(value = "maxDate", defaultValue="") String maxDate 
+			) {
+		
+		text = URL.decodeParam(text);
+		Date min = URL.convertDate(minDate, new Date(0L));
+		Date max = URL.convertDate(maxDate, new Date());
+			
+		List<Post> list = service.fullSearch(text, min, max);
+		return ResponseEntity.ok().body(list);
 
+	}
 }
