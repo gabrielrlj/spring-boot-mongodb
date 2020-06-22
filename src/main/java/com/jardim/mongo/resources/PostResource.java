6ff7f054a2b41fbf0906b61e952380299bb00384
Gabriel Jardim <gabrielrlj99@outlook.com>
2020-06-22 09:48:36 -0300

    consulta simples com query methods

--- src/main/java/com/jardim/mongo/resources/PostResource.java
+++ src/main/java/com/jardim/mongo/resources/PostResource.java
@@ -1,13 +1,17 @@
 package com.jardim.mongo.resources;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.jardim.mongo.domain.Post;
+import com.jardim.mongo.resources.utils.URL;
 import com.jardim.mongo.services.PostService;
 
 @RestController
@@ -16,11 +20,20 @@ public class PostResource {
 
 	@Autowired
 	private PostService service;
+
+	@GetMapping("/{id}")
+	public ResponseEntity<Post> findById(@PathVariable String id) {
+		Post obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);
+	}
+
+	@GetMapping("/titlesearch")
+	public ResponseEntity<List<Post>> findByTitle(@RequestParam(value = "text", defaultValue="") String text) {
 		
-		@GetMapping("/{id}")
-		public ResponseEntity<Post> findById(@PathVariable String id){
-			Post obj = service.findById(id);
-			return ResponseEntity.ok().body(obj);
-		}
-	
+		text = URL.decodeParam(text);
+		List<Post> list = service.findByTitle(text);
+		return ResponseEntity.ok().body(list);
+
+	}
+
 }
